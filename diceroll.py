2f837a04b64d042e6eb76bec42cd2a8224f83fd9
Sebastian Rüth <sebiruet@gmail.com>
2020-01-20 20:51:02 +0100

    Added repeat

--- diceroll.py
+++ diceroll.py
@@ -1,13 +1,15 @@
 import random
 import math
+repeat = "J"
 
-dice_sides = int(input("How many sides should the dice have?: "))
-throw_amount = int(input("How often should the dice be rolled?: "))
-counter = 1
+while (repeat == "J") or (repeat == "j"):
+      dice_sides = int(input("How many sides should the dice have?: "))
+      throw_amount = int(input("How often should the dice be rolled?: "))
+      counter = 1
 
-while throw_amount > 0:
-    stringcounter = str(counter)
-    print("The result of the", stringcounter +
-          ". dice throw is: ", random.randrange(1, dice_sides))
-    counter = counter + 1
-    throw_amount = throw_amount - 1
+      while throw_amount > 0:
+            stringcounter = str(counter)
+            print("The result of the", stringcounter + ". dice throw is: ", random.randrange(1, dice_sides))
+            counter = counter + 1
+            throw_amount = throw_amount - 1
+      repeat = str(input("Do you want to throw again? (J/N): "))
