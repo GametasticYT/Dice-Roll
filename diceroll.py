0bf00d0632a0f8e95588379d7d13c188ca5d69a7
Sebastian Rüth <sebiruet@gmail.com>
2020-01-20 20:36:03 +0100

    Added code

--- diceroll.py
+++ diceroll.py
@@ -1,2 +1,12 @@
 import random
 import math
+
+dice_sides = int(input("How many sides should the dice have?: "))
+throw_amount = int(input("How often should the dice be rolled?: "))
+
+while throw_amount > 0:
+	counter = 1
+	print("The result of the " + counter +
+	      ". dice throw is: " + random.randrange(1, dice_sides))
+	counter = counter + 1
+    throw_amount = throw_amount - 1
